51304e60a4ce9bb37cdfd445e1f4297baf752789
Joey <joezzles@gmail.com>
2023-08-24 17:16:41 -0700

    Create function to collect player input

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
 //Computer randomly chooses between Rock, Paper, Scissors
-let computerChoice = "";
+let computerChoice = getComputerChoice();
 
 function getComputerChoice () {
 
     let randomNumber = Math.floor(Math.random() * 100 + 1);
     if (randomNumber <= 33) {
-        computerChoice = "Rock";
+        return "rock";
     } else if (randomNumber <= 66) {
-        computerChoice = "Paper";
+        return "paper";
     } else {
-        computerChoice = "Scissors";
+        return "scissors";
     }
+    
 }
 
-getComputerChoice();
+// console.log(computerChoice);
+
 //Player inputs Rock, Paper, or Scissors
+
+let playerChoice = getPlayerChoice();
+
+function getPlayerChoice() {
+
+    let playerThrow = prompt("Throw Your Hand, Mortal");
+    playerThrow = playerThrow.toLowerCase();
+
+    if (playerThrow === "rock" || playerThrow === "paper" || playerThrow === "scissors") {
+        return playerThrow;
+    } else {
+        alert("Invalid input! Refresh and try again!")
+    }
+
+}
+
+console.log(playerChoice);
+
 //Code compares computer choice and player input to determine winner
+
+function playRound(playerChoice, computerChoice) {
+    
+}
+
 //Print Win or Lose
