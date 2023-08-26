521520d9161bc0a3a0626fa0e4b6cabd582b1317
Joey <joezzles@gmail.com>
2023-08-25 20:46:21 -0700

    Create working play function and loop it for five rounds

--- script.js
+++ script.js
@@ -1,44 +1,68 @@
-//Computer randomly chooses between Rock, Paper, Scissors
-let computerChoice = getComputerChoice();
+function game(){
 
-function getComputerChoice () {
+    //Loop the code 5 times to play 5 rounds total
 
-    let randomNumber = Math.floor(Math.random() * 100 + 1);
-    if (randomNumber <= 33) {
-        return "rock";
-    } else if (randomNumber <= 66) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
+  for (let i = 0; i < 5; i++){
+
+    //Computer randomly chooses between Rock, Paper, Scissors
     
-}
+    let computerChoice = getComputerChoice();
 
-// console.log(computerChoice);
+    function getComputerChoice () {
+
+        let randomNumber = Math.floor(Math.random() * 100 + 1);
+        if (randomNumber <= 33) {
+            return "rock";
+        } else if (randomNumber <= 66) {
+            return "paper";
+        } else {
+            return "scissors";
+        }
+        
+    }
+
+    console.log(computerChoice);
+
 
 //Player inputs Rock, Paper, or Scissors
 
-let playerChoice = getPlayerChoice();
+    let playerChoice = getPlayerChoice();
 
-function getPlayerChoice() {
+    function getPlayerChoice() {
 
-    let playerThrow = prompt("Throw Your Hand, Mortal");
-    playerThrow = playerThrow.toLowerCase();
+        let playerThrow = prompt("Throw Your Hand, Mortal");
+        playerThrow = playerThrow.toLowerCase();
 
-    if (playerThrow === "rock" || playerThrow === "paper" || playerThrow === "scissors") {
-        return playerThrow;
-    } else {
-        alert("Invalid input! Refresh and try again!")
+        if (playerThrow === "rock" || playerThrow === "paper" || playerThrow === "scissors") {
+            return playerThrow;
+        } else {
+            alert("Invalid input! Try Again!");
+            getPlayerChoice();
+        }
+
     }
 
+    //Evaluate winner
+
+    function playRound(playerChoice, computerChoice) {
+        if (playerChoice === "rock" && computerChoice === "paper") {
+            alert("You lose! Paper beats rock");
+        } else if (playerChoice === "rock" && computerChoice === "scissors") {
+            alert("You win! Rock beats scissors");
+        } else if (playerChoice === "paper" && computerChoice === "rock") {
+            alert("You win! Paper beats rock");
+        } else if (playerChoice === "paper" && computerChoice === "scissors") {
+            alert("You lose! Scissors beats paper");
+        } else if (playerChoice === "scissors" && computerChoice === "rock") {
+            alert("You lose! Rock beats scissors");
+        } else if (playerChoice === "scissors" && computerChoice === "paper") {
+            alert("You win! Scissors beats paper");
+        } else {
+            alert("It's a draw!");
+        }
+    }
+    playRound(playerChoice, computerChoice);
+  }
 }
 
-console.log(playerChoice);
-
-//Code compares computer choice and player input to determine winner
-
-function playRound(playerChoice, computerChoice) {
-    
-}
-
-//Print Win or Lose
+game();
